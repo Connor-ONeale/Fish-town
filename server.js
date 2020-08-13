08e631b7a8a53f4618188bb772de6c6879287003
lex <lexx_stokes@live.com>
2020-08-14 09:17:18 +1200

    server start

---
+++ server.js
@@ -0,0 +1,22 @@
+const express= require('express')
+const router = express.Router()
+const fs = require('fs')
+const { request } = require('http')
+
+router.get('/', (req, res) => {
+    fs.readFile('./data.json', 'utf8' ,(err, data) => {
+        if(err) throw err 
+        const obj = JSON.parse(data)
+    res.render("fish/index", obj)
+  })
+})
+
+router.get('/fish', (req, res) => {
+    fs.readFile('./data.json', 'utf8' ,(err, data) => {
+        if(err) throw err 
+        const obj = JSON.parse(data)
+  res.render('fish/index', obj)
+})
+})
+
+module.exports = router
